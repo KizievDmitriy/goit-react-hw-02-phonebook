ffe667389e78af51acc7ea761e6f3699cf65a11f
KizievDmitriy <kizievdmitrii@gmail.com>
2022-07-02 18:26:49 +0300

    rebuild

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,16 @@
 export const App = () => {
   return (
-    <div>
-      <h1>Phonebook</h1>
-      <h2>Contacts</h2>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
     </div>
   );
 };

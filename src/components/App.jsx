fd03a491e6e3d91faaff9188067926279df26ebe
KizievDmitriy <kizievdmitrii@gmail.com>
2022-07-02 18:12:59 +0300

    remove

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,6 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        color: '#010101'
-      }}
-    >
+    <div>
       React homework templatett
     </div>
   );
